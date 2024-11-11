Bugfix: correct LCOE units in OpenMDAO HOPP wrapper (#43)
* correct units for lcoe_real in HOPPComponent

greenheart/tools/optimization/openmdao.py
@@ -182,7 +182,7 @@ class HOPPComponent(om.ExplicitComponent):
         self.add_output("hybrid_electrical_generation_capex", units="USD")
         self.add_output("hybrid_electrical_generation_opex", units="USD")
         self.add_output("aep", units="kW*h")
-        self.add_output("lcoe_real", units="USD/(MW*h)")
+        self.add_output("lcoe_real", units="USD/(kW*h)")
         self.add_output("power_signal", units="kW", val=np.zeros(8760))
 
     def compute(self, inputs, outputs):