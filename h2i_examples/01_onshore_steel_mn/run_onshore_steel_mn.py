7954c9ea459d286ca0f2334bafcda681266bc760
John Jasa <johnjasa11@gmail.com>
2025-03-13 13:56:35 -0600

    Added missing example

---
+++ h2i_examples/01_onshore_steel_mn/run_onshore_steel_mn.py
@@ -0,0 +1,10 @@
+from greenheart.core.greenheart_model import GreenHEARTModel
+
+
+# Create a GreenHEART model
+gh = GreenHEARTModel("08_onshore_steel_mn.yaml")
+
+# Run the model
+gh.run()
+
+gh.post_process()
