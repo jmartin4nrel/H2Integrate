1c6a377477fb2ccd320fb6cee5ecada781245ec6
jmartin4 <jonathan.martin@nrel.gov>
2025-04-14 15:34:59 -0600

    Numbering methanol example

--- h2i_examples/03_methanol/run_methanol.py
+++ h2i_examples/03_methanol/run_methanol.py
@@ -2,7 +2,7 @@ from h2integrate.core.h2integrate_model import H2IntegrateModel
 
 
 # Create a GreenHEART model
-h2i = H2IntegrateModel("0X_methanol.yaml")
+h2i = H2IntegrateModel("03_methanol.yaml")
 
 # Run the model
 h2i.run()
