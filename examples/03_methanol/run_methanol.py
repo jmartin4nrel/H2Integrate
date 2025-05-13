6cbddc72d84ee6e6990bb25bf7eb151d17cb36ee
jmartin4 <jonathan.martin@nrel.gov>
2025-05-13 17:03:12 -0600

    Separating example .yaml

--- examples/03_methanol/run_methanol.py
+++ examples/03_methanol/run_methanol.py
@@ -1,8 +1,9 @@
 from h2integrate.core.h2integrate_model import H2IntegrateModel
 
 
-# Create an H2I model
+# Create an H2I model - comment out one line to select SMR or CO2 hydrogenation
 h2i = H2IntegrateModel("03_smr_methanol.yaml")
+h2i = H2IntegrateModel("03_co2h_methanol.yaml")
 
 # Run the model
 h2i.run()
